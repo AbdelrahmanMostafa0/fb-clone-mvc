c7924817eacb748f24977e5f8b20b8dd7c6fccb0
Your Name <you@example.com>
2022-04-25 08:05:45 +0200

    uploding for the first time

---
+++ fb-clone-mvc/Controllers/homeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fb_clone_mvc.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult home()
+        {
+            return View();
+        }
+        public ActionResult profile()
+        {
+            return View();
+        }
+        public ActionResult friends()
+        {
+            return View();
+        }
+        public ActionResult log_in()
+        {
+            return View();
+        }
+        public ActionResult sign_up()
+        {
+            return View();
+        }
+        public ActionResult Search_account()
+        {
+            return View();
+        }
+        public ActionResult view_friend_profile()
+        {
+            return View();
+        }
+
+        public ActionResult view_friend_request_profile()
+        {
+            return View();
+        }
+        public ActionResult edit_profile()
+        {
+            return View();
+        }
+        public ActionResult view_stranger()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ fb-clone-mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="fb_clone_mvc.MvcApplication" Language="C#" %>
